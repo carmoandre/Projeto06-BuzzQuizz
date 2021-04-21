c0ba29dbead079816452c037afe204f42706c661
André Carmo <andre_de11@msn.com>
2021-04-21 20:58:27 -0300

    Primeiras implementações das funções de JS para a tela 01.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,3 +1,49 @@
+const lists = document.querySelectorAll(".quizzes");
+
 function temp() {
     console.log("Funciona! substituir!");
+}
+
+getAllQuizzes()
+function getAllQuizzes() {
+    const request = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v2/buzzquizz/quizzes");
+
+    request.then(renderWithAnswer);
+    request.catch(gettingAllQuizzesError);
+}
+
+function renderWithAnswer(answer){
+    renderQuizzes(answer.data);
+}
+
+function gettingAllQuizzesError(answer) {
+    alert(`Erro ao tentar recuperar os Quizess! Status: ${answer.response.status}. Por favor, recarregue a página e tente de novo`);
+}
+
+function renderQuizzes(quizzesList) {
+    resetQuizzesLists();
+    quizzesList.forEach(renderQuizz);
+}
+
+function resetQuizzesLists() {
+    lists[0].innerHTML= "";
+    lists[1].innerHTML= "";
+}
+
+function renderQuizz(quizzInfo) {
+    //TODO comparação com os ids dos meus quizz pra distribuir entre as listas
+    lists[1].innerHTML += `
+        <li id="${quizzInfo.id}">
+            <p>${quizzInfo.title}</p>
+        </li>
+    `;
+    document.getElementById(quizzInfo.id).style.background = `
+    linear-gradient(
+        180deg,
+        rgba(255, 255, 255, 0) 0%,
+        rgba(0, 0, 0, 0.5) 64.58%,
+        #000000 100%
+      ),
+      url(${quizzInfo.image})
+    `;
 }
