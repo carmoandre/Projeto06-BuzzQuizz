8355eb5b2813448365e06734bce185d3cb24995e
Adriano Gotardo <adrianogotardo.12@gmail.com>
2021-04-22 16:08:49 -0300

    Implementação de todos os recursos da Tela 2 anteriores à tela de resultado

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,7 +1,14 @@
+history.scrollRestoration = "manual";
+
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+}
+
 const lists = document.querySelectorAll(".quizzes");
 const homeScreenElement = document.querySelector(".homeScreen");
 const quizzInsideScreenElement = document.querySelector(".quizzInsideScreen");
 const createQuizzScreenElement = document.querySelector(".createQuizzScreen");
+let atualQuestion;
 
 function temp() {
     console.log("Funciona! substituir!");
@@ -56,6 +63,7 @@ function toQuizzInsideScreenTransition(quizzID) {
     getOneQuizz(quizzID);
     homeScreenElement.classList.toggle("hiddingClass");
     quizzInsideScreenElement.classList.toggle("hiddingClass");
+    window.scrollTo(0, 0);
 }
 
 function getOneQuizz(quizzID) {
@@ -67,22 +75,49 @@ function getOneQuizz(quizzID) {
 
 function openQuizz(response) {
     let selectedQuizz = response.data;
-    console.log(selectedQuizz);
     let finalHTML = `<div class='quizzHeader'><p>${selectedQuizz.title}</p></div><div class='quizzContent'>`;
+
+    //IMPLEMENTAÇÃO DO HTML DA PÁGINA 2
     for (let i=0; i<selectedQuizz.questions.length; i++) {
-        finalHTML += `<ul class='quizzQuestion'><li><p>${selectedQuizz.questions[i].title}</p></li>`;
+        finalHTML += `<ul onclick="scrollPage(this)" class='quizzQuestion' id='question${i}'><li style='background-color: ${selectedQuizz.questions[i].color}'><p>${selectedQuizz.questions[i].title}</p></li>`;
         let answers = selectedQuizz.questions[i].answers;
-        console.log(answers);
         shuffleAnswers(answers);
         for (let j=0; j<answers.length; j++) {
-            finalHTML += `<li><p>${answers[j].text}</p></li>`
+            console.log(answers[j].isCorrectAnswer);
+            finalHTML += `<li value=${answers[j].isCorrectAnswer} id='possibleAnswer' name='answer${j}'><img src='${answers[j].image}' width="330px" height="175px"><h1>${answers[j].text}</h1></li>`
         }
         finalHTML += `</ul>`
         document.querySelector('.quizzInsideScreen').innerHTML = finalHTML;
     }
     document.querySelector('.quizzHeader').style.backgroundImage = `linear-gradient(0deg, rgba(0, 0, 0, 0.57), rgba(0, 0, 0, 0.57)), url(${selectedQuizz.image})`;
     document.querySelector('.quizzInsideScreen').innerHTML += `</div>`;
+
+    //IMPLEMENTAÇÃO DA FUNÇÃO QUE ALTERA A OPACIDADE DAS ALTERNATIVAS NÃO-SELECIONADAS & COLORE OS TEXTOS DAS ALTERNATIVAS
+    let possibleAnswers = quizzInsideScreenElement.querySelectorAll('#possibleAnswer');
+    console.log(possibleAnswers);
+    for (let i=0; i<possibleAnswers.length; i++) {
+        possibleAnswers[i].onclick = function() { 
+            let answeredQuestion = this.parentElement.querySelectorAll('#possibleAnswer');
+            let verifier = this;
+            for (let k=0; k<answeredQuestion.length; k++) {
+                console.log(answeredQuestion[k]);
+                console.log(answeredQuestion[k].attributes.value);
+                let greenOrRed = answeredQuestion[k].attributes.getNamedItem("value").value;
+                if (greenOrRed == 'true') {
+                    answeredQuestion[k].style.color = 'green';
+                } else {
+                    answeredQuestion[k].style.color = 'red';
+                }
+                if (answeredQuestion[k] == verifier) {}
+                else if (answeredQuestion[k].name != `answer${possibleAnswers[i].name}`) {
+                    answeredQuestion[k].classList.add('whiteShade');
+                    answeredQuestion[k].onclick = null;
+                }
+            }
+        }
+    }
 }
+
 
 function gettingOneQuizError(answer) {
     alert(`Erro ao tentar recuperar o quizz desejado! Status: ${answer.response.status}. Por favor, recarregue a página e tente de novo`);
@@ -93,9 +128,19 @@ function shuffleAnswers(array) {
         const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
     }
-    console.log(array);
+    answers = array;
 }
 
-function clickedAnswer() {
-    
+function scrollPage(question) {
+    atualQuestion = question;
+    setTimeout(scrollToNextQuestion, 2000)
+}
+
+function scrollToNextQuestion() {
+    let nextPosition = atualQuestion.nextSibling.offsetTop - 50;
+    let scrollOptions = {
+        top: nextPosition,
+        behavior: 'smooth'
+    }
+    window.scrollTo(scrollOptions);
 }
